657cfa4e12c27351126d901567920e698e26623c
Pramod Abhiram Mutyala <mutyalapramodabhiram@gmail.com>
2026-08-30 19:26:48 +0530

    fix: configure tile layer subdomains and attribution

--- frontend/src/components/HeatMap.jsx
+++ frontend/src/components/HeatMap.jsx
@@ -73,8 +73,9 @@ export default function HeatMap({
 
         {/* Dark CARTO Basemap Tiles */}
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a> | Data: FortyGuard'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a> | Data: FortyGuard'
           url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          subdomains="abcd"
           maxZoom={19}
         />
 
--- frontend/src/components/InteractiveHeatMap.jsx
+++ frontend/src/components/InteractiveHeatMap.jsx
@@ -227,8 +227,10 @@ export default function InteractiveHeatMap({
             
             {/* CARTO Dark Matter Basemap */}
             <TileLayer
-              attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
               url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              subdomains="abcd"
+              maxZoom={20}
             />
 
             {/* Render GeoJSON Polygons */}
